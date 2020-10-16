3ca5f0f2bb8664897357336967c3f69e2d45032e
GameDevloper777 <pranavrauthan@gmail.com>
2020-10-16 16:59:23 +0530

    changes donegit add Slingshot.js sketch.js

--- Slingshot.js
+++ Slingshot.js
@@ -8,6 +8,9 @@ class SlingShot{
         }
         this.pointB = pointB
         this.sling = Constraint.create(options);
+        this.sling1 = loadImage("sprites/sling1.png");
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png"); 
         World.add(world, this.sling);
     }
 
@@ -19,9 +22,21 @@ class SlingShot{
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
+            stroke(71,35,0)
             strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            if(pointA.x<180){
+            line(pointA.x-20, pointA.y+15, pointB.x+30, pointB.y-25);
+            line(pointA.x-20, pointA.y+15, pointB.x-10, pointB.y-25);
+            image(this.sling3,pointA.x-25,pointA.y,10,20);
         }
+        else{
+            line(pointA.x+20, pointA.y+15, pointB.x+30, pointB.y-25);
+            line(pointA.x+20, pointA.y+15, pointB.x-10, pointB.y-25);
+            image(this.sling3,pointA.x+25,pointA.y,10,20);
+        }
+            image(this.sling1,200,25);
+            image(this.sling2,170,25);
+        }    
     }
     
 }
